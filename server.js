3e70ea9515fd64c96ec40f29c7dc5d676e815cd7
Cris M <cristian.montenegro.p@gmail.com>
2020-08-23 08:02:49 -0700

    Deploying to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
-const routes = require("./routes/routes");
+// const routes = require("./routes/routes");
+const router = require("express").Router();
 
 var PORT = process.env.PORT || 3000;
 
@@ -10,7 +11,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 app.use(express.static('build'));
-app.get("/", function (req, res) {
+router.use(function (req, res) {
   res.sendFile(path.join(__dirname, "../../build/index.html"));
 });
 
