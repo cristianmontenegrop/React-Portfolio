6ac30bd57afdf74baf77900c0bcf0ec276fa548d
Cris M <cristian.montenegro.p@gmail.com>
2020-08-23 08:55:45 -0700

    Deploying to Heroku

---
+++ public/precache-manifest.7934c91987eba11bfcef20b947acbb7e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a355262bac101451e29668e2521d7c8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ffb653e278bb8eb0684",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5bdff5ff9c3976c9451f",
+    "url": "/static/css/main.ffc159f4.chunk.css"
+  },
+  {
+    "revision": "2ffb653e278bb8eb0684",
+    "url": "/static/js/2.c9f14f2e.chunk.js"
+  },
+  {
+    "revision": "861d2ba538e18c9ff1d81680ed865d7f",
+    "url": "/static/js/2.c9f14f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bdff5ff9c3976c9451f",
+    "url": "/static/js/main.f99c3227.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "845374f4c27bedab8dc6c1ea9a476518",
+    "url": "/static/media/cristian-image.845374f4.png"
+  },
+  {
+    "revision": "79bef34db3d148201f07067245c07984",
+    "url": "/static/media/eat-da-burger-image.79bef34d.png"
+  },
+  {
+    "revision": "b82c35b5c725b904776733c07eb0e07e",
+    "url": "/static/media/employee-manager-image.b82c35b5.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "133001ecfb7dabe9f02eff01cc29665a",
+    "url": "/static/media/note-taker-image.133001ec.png"
+  },
+  {
+    "revision": "51a93d5044744c29630af2bb666028a5",
+    "url": "/static/media/resume.51a93d50.pdf"
+  },
+  {
+    "revision": "6307eada9dc2bc84188d85c3157c54b4",
+    "url": "/static/media/template-engine-employee-summary-image.6307eada.png"
+  },
+  {
+    "revision": "0e19f4be7ae3c386381608414f147b64",
+    "url": "/static/media/travex-image.0e19f4be.png"
+  },
+  {
+    "revision": "a41cc66e324ff619d6726cab2fa88771",
+    "url": "/static/media/wheres-my-veggie-image.a41cc66e.png"
+  }
+]);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var path = require("path");
 // const routes = require("./routes/routes");
-const router = require("express").Router();
+// const router = require("express").Router();
 
 var PORT = process.env.PORT || 3000;
 
@@ -12,9 +12,10 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 app.use(express.static('build'));
-router.use(function (req, res) {
-  res.sendFile(path.join(__dirname, "./build/"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
